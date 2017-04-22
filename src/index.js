5714c8284a1efc5f2771f6055da169fd31b4ea8d
steelydylan <ess_president@me.com>
2017-04-22 10:02:16 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -26,9 +26,9 @@ class App extends React.Component {
         <Section title="次のイベント" text="">
           <LinkCard
             src="https://dzpp79ucibp5a.cloudfront.net/assets/doorkeeper_group_normal-125b448b722fa8c158516cf4b86aafda26b442af55a001418b0eb2acf7117961.gif"
-            title="Nagoya.js #2"
+            title="Nagoya.js #3"
             text="名古屋のフロントエンドの仲間を増やすためのJavaScript勉強会です。"
-            href="https://nagoya-js.doorkeeper.jp/events/56872"
+            href="https://nagoya-js.doorkeeper.jp/events/59850"
           />
         </Section>
         <Section title="お問い合わせ" text="">
